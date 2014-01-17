c7529e0d7a21f3be48febeb2a9bc860966f2543e
Dale Hopkins <dhopkins@vendasta.com>
2014-01-17 13:27:30 -0600

    Start on player list

--- src/app/sc2/views/player.py
+++ src/app/sc2/views/player.py
@@ -1,6 +1,9 @@
 """
 players.py Documentation
 """
+import json
+from app.sc2.models.player import PlayerModel
+from app.sc2.utils import jsonDateTimeHandler
 from app.sc2.views import UserView
 
 
@@ -13,5 +16,9 @@ class PlayerIndex(UserView):
         See above
         """
         data = {}
+
+        #Grab the list of players
+        players = PlayerModel.query().fetch()
+        data['playersJson'] = json.dumps(players, default=jsonDateTimeHandler)
 
         self.render_response('sc2/players/index.html', **data)
