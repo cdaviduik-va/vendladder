6a9672e32887d1223d3539baa263a9ba4cdc03b7
Chris Daviduik <cdaviduik@vendasta.com>
2014-09-05 15:58:37 -0400

    match-season-improvements Match stats are now calculated as each replay is uploaded instead of only when closed. Creating a new season now closes the currently open one and transfers players and their ranks. Now displaying list of recent matches (in addition to games) on match history page.

--- src/app/sc2/domain/season.py
+++ src/app/sc2/domain/season.py
@@ -2,7 +2,8 @@
 Do stuff with seasons
 """
 import datetime
-from google.appengine.ext.ndb import Key
+from google.appengine.ext.ndb import Key, put_multi
+from app.sc2.models.player import PlayerModel, PlayerRankModel
 from app.sc2.models.season import SeasonModel
 
 
@@ -10,14 +11,28 @@ def create_season(season_name, start_date):
     """
     Creates a season
     """
-    if SeasonModel.lookup_open():
-        raise ValueError('Cannot create a new season when one is already open. You must end the current season first.')
+    last_season = SeasonModel.lookup_open()
+    close_current_season()
 
     date_pieces = start_date.split("-")
     real_start_date = datetime.date(int(date_pieces[0]), int(date_pieces[1]), int(date_pieces[2]))
     key = SeasonModel.generate_key(season_name, real_start_date)
     season = SeasonModel(key=key, season_name=season_name, start_date=real_start_date)
-    return season.put()
+    season.put()
+
+    if last_season:
+        players = PlayerModel.lookup_for_season(last_season.season_id)
+        for player in players:
+            player.seasons_participated.append(season.season_id)
+            last_player_rank = PlayerRankModel.get_or_create(player.battle_net_name, last_season.season_id)
+            if last_player_rank:
+                player_rank = PlayerRankModel.get_or_create(player.battle_net_name, season.season_id)
+                player_rank.score = last_player_rank.score
+                player_rank.put()
+
+        put_multi(players)
+
+    return season
 
 
 def lookup_seasons():
--- src/app/sc2/views/match.py
+++ src/app/sc2/views/match.py
@@ -1,5 +1,5 @@
 import json
-from app.sc2.domain.match import lookup_games, lookup_open_matches, close_match
+from app.sc2.domain.match import lookup_games, lookup_open_matches, close_match, lookup_matches
 from app.sc2.utils import jsonDateTimeHandler
 from app.sc2.views import UserView
 
@@ -16,7 +16,8 @@ class MatchHistoryView(UserView):
         limit = int(limit)
         data = {
             'current_matches': lookup_open_matches(),
-            'games': json.dumps([game.to_dict() for game in lookup_games(limit=limit)], default=jsonDateTimeHandler)
+            'games': json.dumps([game.to_dict() for game in lookup_games(limit=limit)], default=jsonDateTimeHandler),
+            'matches': lookup_matches(limit=10)
         }
         self.render_response('sc2/match/history.html', **data)
 
